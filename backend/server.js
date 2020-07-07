bc8e061e906e55cbd8d99d9902d9686b88d51cdc
Jackson <jackson.kelley.g@gmail.com>
2020-07-06 21:40:25 -0700

    Front end user form posts to the users endpoint in the database, and the dropdown creates options based off of the contents in the users collection of the mongodb database

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, options)
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB database connection established successfully')
+})
+
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class EditExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Edit Exercises</p>
+            </div>
+        )
+    }
+}
+
+export default EditExercises
